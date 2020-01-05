9c6eec7c72587c07091e1b4327ca8bdee18a9014
James Swineson <github@public.swineson.me>
2020-01-05 13:01:10 -0800

    add multiple image formats

--- 50-pack.sh
+++ 50-pack.sh
@@ -7,5 +7,7 @@ IMAGE=debian.img
 
 cd "$ROOT"
 tar -cvzf "$IMAGE".tar.gz "$IMAGE"
-
+qemu-img convert -f raw -O qcow2 "$IMAGE" "$IMAGE".qcow2
+qemu-img convert -f raw -O vmdk -o adapter_type=lsilogic,subformat=streamOptimized,compat6 "$IMAGE" "$IMAGE".vmdk
+qemu-img convert -f raw -O vhdx "$IMAGE" "$IMAGE".vhdx
 ls -alh 
