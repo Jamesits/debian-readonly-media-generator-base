cd1d434576f6f5bc4263116935b7071472c54db0
James Swineson <github@public.swineson.me>
2020-01-05 07:11:06 -0800

    implement rootfs override

--- 30-rootfs.sh
+++ 30-rootfs.sh
@@ -3,7 +3,26 @@ set -Eeuo pipefail
 set -x
 
 ROOT=build
+export LANG=C.UTF-8
+rm -rf --one-file-system "$ROOT"/debinst
+rm -f "$ROOT"/rootfs.squashfs
 mkdir -p "$ROOT"/debinst
-debootstrap --arch amd64 stretch "$ROOT"/debinst http://ftp.us.debian.org/debian
-mksquashfs "$ROOT"/debinst "$ROOT"/rootfs.squashfs -comp xz
+
+# install a minimal OS
+debootstrap --arch amd64 buster "$ROOT"/debinst http://ftp.us.debian.org/debian
+
+# apply overrides
+chown -R root:root rootfs_overrides
+cp -rv rootfs_overrides/* "$ROOT"/debinst/
+
+# fix things
+chroot "$ROOT"/debinst passwd -d root
+
+# remove apt cache
+chroot "$ROOT"/debinst apt-get clean
+chroot "$ROOT"/debinst apt-get autoremove
+rm -rf "$ROOT"/debinst/var/lib/apt/lists/*
+
+# pack rootfs
+mksquashfs "$ROOT"/debinst "$ROOT"/rootfs.squashfs -comp xz -noappend
 ls -alh "$ROOT"/rootfs.squashfs
