b3581be2e6a1b76128ae0b29406ffb4e0fa2657e
James Swineson <github@public.swineson.me>
2020-01-05 09:03:53 -0800

    fix deps problem on ubuntu 16.04

--- 00-install-deps.sh
+++ 00-install-deps.sh
@@ -3,4 +3,4 @@ set -Eeuo pipefail
 set -x
 
 apt-get update -y
-apt-get install -y qemu-utils grub2-common dosfstools gdisk wget tar debootstrap grub-efi-amd64-signed grub-pc
+apt-get install -y qemu-utils grub2-common dosfstools gdisk wget tar debootstrap grub-efi-amd64-signed grub-pc grub-efi-amd64
