9c7f943f2d3f09d08acbd89747b692d8c647b670
James Swineson <github@public.swineson.me>
2020-01-05 07:23:47 -0800

    fix the input device is not a TTY

--- 20-kernel-initramfs.sh
+++ 20-kernel-initramfs.sh
@@ -5,5 +5,5 @@ set -x
 ROOT=build
 
 mkdir -p "$ROOT"/boot
-docker run --rm -it -v $(realpath docker):/mnt/builder:ro -v $(realpath "$ROOT"/boot):/mnt/build debian:buster-slim /mnt/builder/00-initramfs.sh
+docker run --rm -i -v $(realpath docker):/mnt/builder:ro -v $(realpath "$ROOT"/boot):/mnt/build debian:buster-slim /mnt/builder/00-initramfs.sh
 ls -alh "$ROOT"/boot
