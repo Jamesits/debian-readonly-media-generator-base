d8a1f21ead81800d3aab66a8586276255ed7e749
James Swineson <github@public.swineson.me>
2020-01-05 22:17:23 -0800

    add apt non-free sources to docker image

--- 20-kernel-initramfs.sh
+++ 20-kernel-initramfs.sh
@@ -5,5 +5,9 @@ set -x
 ROOT=build
 
 mkdir -p "$ROOT"/boot
-docker run --rm -i -v $(realpath docker):/mnt/builder:ro -v $(realpath "$ROOT"/boot):/mnt/build debian:buster-slim /mnt/builder/00-initramfs.sh
+docker run --rm -i \
+	-v $(realpath docker):/mnt/builder:ro \
+	-v $(realpath "$ROOT"/boot):/mnt/build \
+	-v $(realpath rootfs_overrides/etc/apt/sources.list):/etc/apt/sources.list:ro \
+	debian:buster-slim /mnt/builder/00-initramfs.sh
 ls -alh "$ROOT"/boot
