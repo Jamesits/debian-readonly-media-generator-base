43d2435acf90adeb8328cb61f8b6c475a5539c6e
James Swineson <github@public.swineson.me>
2020-01-27 13:08:50 +0000

    Enable kernel lockdown
    https://lwn.net/Articles/750730/

--- 40-image.sh
+++ 40-image.sh
@@ -6,7 +6,7 @@ ROOT=build
 IMAGE=debian.img
 KERNEL_ARGS_FAST="noibrs noibpb nopti nospectre_v2 nospectre_v1 l1tf=off nospec_store_bypass_disable no_stf_barrier mds=off tsx=on tsx_async_abort=off mitigations=off"
 KERNEL_ARGS_LIVE="boot=live forcefsck ignore_uuid live-media-path=/system nopersistence swap=true noeject" 
-KERNEL_ARGS_MISC="console=ttyS0,9600 console=tty1"
+KERNEL_ARGS_MISC="console=ttyS0,9600 console=tty1 lockdown"
 KERNEL_ARGS_NORM="panic=5"
 GRUB_MODULES="nativedisk biosdisk pci ata pata scsi ahci usb ehci uhci usbms disk part_gpt fat file configfile search search_fs_file echo ls reboot usb_keyboard at_keyboard"
 
